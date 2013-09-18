b231160da688441229edddfb11ea1ccca004d7da
Avidanborisov <Avidanborisov@gmail.com>
2013-09-18 18:27:44 +0300

    Update documentation

--- search/all_61.js
+++ search/all_61.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['addconstant',['addConstant',['../class_expressio_1_1_evaluator.html#a3d8a3c0fced77a4010d69c243bc6b79a',1,'Expressio::Evaluator']]],
   ['addfunction',['addFunction',['../class_expressio_1_1_evaluator.html#a69f505e9ad01ebc161bd4ec18cf474ce',1,'Expressio::Evaluator']]]
 ];
